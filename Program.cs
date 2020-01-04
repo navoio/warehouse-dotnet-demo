f61ad1f16d6b05afc7348e43c48934842ef6db66
Dominic Foti <dominic@fotijr.com>
2020-01-03 20:57:27 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Navo.Client;
+
+namespace WarehouseDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Navo Warehouse Client Demo");
+            var apiUrl = Environment.GetEnvironmentVariable("NAVO_API");
+            var clientId = Guid.Parse(Environment.GetEnvironmentVariable("NAVO_CLIENT"));
+            var apiKey = Environment.GetEnvironmentVariable("NAVO_API_KEY");
+            Console.WriteLine($"Connecting to { apiUrl }...");
+            var client = new NavoClient(apiUrl);
+            await client.AuthenticateAsync(clientId, apiKey);
+            Console.WriteLine("Navo Client authorized.");
+            var jobs = await client.Jobs.GetAsync();
+            Console.WriteLine($"{ jobs.Count } jobs found.");
+            if (jobs.Count == 0)
+            {
+                Console.WriteLine("No jobs found, skipping demonstration of getting job details.");
+                return;
+            }
+            var job = await client.Jobs.GetAsync(jobs[0].Id);
+            Console.WriteLine($"Job { job.Name } requires { job.TotalParts } parts.");
+            Console.WriteLine("Warehouse dotnet client demo successfully ran.");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Navo Warehouse .NET Client [![Actions Status](https://github.com/navoio/warehouse-dotnet-demo/workflows/dotnet-core/badge.svg)](https://github.com/navoio/warehouse-dotnet-demo/actions)
+This project demonstrates how to use the Navo Warehouse Client in C#.
+
+## Running the demo
+To run the demo, set the following environment variables:
+- `NAVO_API`: Base url of the Navo API being accessed.
+- `NAVO_CLIENT`: Navo client ID.
+- `NAVO_API_KEY`: API key for the client. 
